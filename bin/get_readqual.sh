8c2927659c42a9d687419cb353ee184581ef5218
rivera10 <riveravicens@gmail.com>
2020-07-12 18:02:45 +0200

    Fix extra name

--- bin/get_readqual.sh
+++ bin/get_readqual.sh
@@ -8,4 +8,4 @@ r1an=$( jq '.read1_after_filtering.quality_curves.mean' ${jfile} | grep -c [0-9]
 r1aq=$( jq '.read1_after_filtering.quality_curves.mean' ${jfile} | grep [0-9] | tr -d "\n" | tr -d " " )
 r2an=$( jq '.read2_after_filtering.quality_curves.mean' ${jfile} | grep -c [0-9] )
 r2aq=$( jq '.read2_after_filtering.quality_curves.mean' ${jfile} | grep [0-9] | tr -d "\n" | tr -d " " )
-echo -e "${sampleid},${r1bn}\n${r1bq}\n${r2bn}\n${r2bq}\n${r1an}\n${r1aq}\n${r2an}\n${r2aq}" >${sampleid}_reads_qual.csv
+echo -e "${r1bn}\n${r1bq}\n${r2bn}\n${r2bq}\n${r1an}\n${r1aq}\n${r2an}\n${r2aq}" >${sampleid}_reads_qual.csv
