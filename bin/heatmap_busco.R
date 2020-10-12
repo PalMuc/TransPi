9557a0d20ebab7e5e7f290f3e991018bb7081611
rivera10 <riveravicens@gmail.com>
2020-10-12 10:42:32 +0200

    Add new heatmap for TransPi missing

--- bin/heatmap_busco.R
+++ bin/heatmap_busco.R
@@ -1,10 +1,13 @@
 args = commandArgs(trailingOnly=TRUE)
 sample_name=args[1]
+comp_table=args[2]
+transpi_table=args[3]
 
 library(plotly)
 library(reshape2)
 
-csv=read.csv(sample_name, header=TRUE, sep="\t")
+# comparison table
+csv=read.csv(comp_table, header=TRUE, sep="\t")
 
 csv <- data.frame(lapply(csv, function(x) {gsub("Complete", "3", x)}))
 csv <- data.frame(lapply(csv, function(x) {gsub("Duplicated", "2", x)}))
@@ -21,7 +24,28 @@ fig <- plot_ly(c,x=~variable, y=~Busco.ID, z=~value, colorscale=colz, reversesca
 fig <- fig %>% layout(xaxis=list(title="", showline = TRUE, mirror = TRUE),
               yaxis=list(title="BUSCO ID", tickmode="auto", nticks=length(csv$Busco.ID),
               tickfont=list(size=8), showline = TRUE, mirror = TRUE))
-fig
 
-orca(fig, paste(sample_name,"_TransPi_BUSCO.png",sep=""))
-orca(fig, paste(sample_name,"_TransPi_BUSCO.pdf",sep=""))
+orca(fig, paste(sample_name,"_all_missing_BUSCO.png",sep=""))
+orca(fig, paste(sample_name,"_all_missing_BUSCO.pdf",sep=""))
+
+# TransPi table
+csv=read.csv(transpi_table, header=TRUE, sep="\t")
+
+csv <- data.frame(lapply(csv, function(x) {gsub("Complete", "3", x)}))
+csv <- data.frame(lapply(csv, function(x) {gsub("Duplicated", "2", x)}))
+csv <- data.frame(lapply(csv, function(x) {gsub("Fragmented", "1", x)}))
+csv <- data.frame(lapply(csv, function(x) {gsub("Missing", "0", x)}))
+csv
+c=melt(csv,id.vars = 'Busco.ID')
+dec=c(0,.25,.25,.50,.50,.75,.75,1)
+my_colors <- c("#081D58","#081D58", "#2280B8","#2280B8", "#99D6B9", "#99D6B9","#f8f9fc","#f8f9fc")
+colz <- setNames(data.frame(dec, my_colors), NULL)
+fig <- plot_ly(c,x=~variable, y=~Busco.ID, z=~value, colorscale=colz, reversescale=T, type = "heatmap",
+              colorbar=list(tickmode='array', tickvals=c(.35,1.1,1.87,2.60), thickness=30,
+              ticktext= c("Missing","Fragmented","Duplicated","Complete"), len=0.4))
+fig <- fig %>% layout(xaxis=list(title="", showline = TRUE, mirror = TRUE),
+              yaxis=list(title="BUSCO ID", tickmode="auto", nticks=length(csv$Busco.ID),
+              tickfont=list(size=8), showline = TRUE, mirror = TRUE))
+
+orca(fig, paste(sample_name,"_TransPi_missing_BUSCO.png",sep=""))
+orca(fig, paste(sample_name,"_TransPi_missing_BUSCO.pdf",sep=""))
